2e9d382827228e44a6cb96f990809287de05d32b
dakatapetrov <daka7a@gmail.com>
2013-02-18 23:50:32 +0200

    Fix database plugin

--- lib/db_plugin/administration.rb
+++ lib/db_plugin/administration.rb
@@ -1,24 +1,17 @@
 require 'data_mapper'
 require_relative '../core/administration'
 
-DataMapper::setup(:default, "sqlite3:///home/dakata/Documents/Development/Ruby/alexandra/db/development.db")
 
 module Alexandra
   module DB
+
+    DataMapper::setup(:default, "sqlite3:///home/dakata/Documents/Development/Ruby/alexandra/db/development.db")
+
     class Administrator < Alexandra::Core::Administrator
       include DataMapper::Resource
       property :id, Serial
       property :email, String, :required => true
       property :password, BCryptHash, :required => true
-    
-      def self.create(id, username, password)
-        administrator = Administrator.new
-    
-        administrator.id, administrator.username = id, username
-        administrator.password = password
-    
-        administrator
-      end
     end
     
     class Administrators < Alexandra::Core::Administrators
@@ -29,23 +22,21 @@ module Alexandra
       def remove(id)
         Administrator.get(id).destroy
       end
+
+      def save
+        @administrators.each { |administrator| administrator.save }
+      end
     end
     
     class Library < Alexandra::Core::Library
       include DataMapper::Resource
       property :id, Serial, :default => 1
       property :name, String, :required => true
-      #property :fine
-    
-      def self.create(name, fine)
-        library = Library.new
-    
-        library.name = name
-        library.fine = fine
-    
-    
-        library
-      end
+      property :fine, Float, :required => true
+
     end
+
+    DataMapper.finalize.auto_upgrade!
+
   end
 end
--- spec/db_plugin/circulation_spec.rb
+++ spec/db_plugin/circulation_spec.rb
@@ -1,2 +1,5 @@
 require 'spec_helper'
 require 'db_plugin/circulation.rb'
+
+module Alexandra::DB
+end
